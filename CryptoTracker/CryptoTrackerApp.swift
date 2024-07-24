b0d9653aea5f8f3dba680bb8e458a3f0588eca22
atallah3 <aratallah3@gmail.com>
2024-07-24 07:37:10 +0300

    init commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Abd Elrahman Atallah on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
