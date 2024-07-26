08de112d168dfc1b6c791d448b626ddb17158ef8
atallah3 <aratallah3@gmail.com>
2024-07-26 15:59:55 +0300

    Add PreviewProvider Extension

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Abd Elrahman Atallah on 26/07/2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin : CoinModel
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct CoinRowView_Preview: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin)
+    }
+}
