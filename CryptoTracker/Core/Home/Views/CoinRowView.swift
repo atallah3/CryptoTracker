f3a2d419f784c37763cdd92a30983a707cf73406
atallah3 <aratallah3@gmail.com>
2024-07-29 07:43:32 +0300

    Add CoinRowView + Double EXT

--- CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -8,15 +8,73 @@
 import SwiftUI
 
 struct CoinRowView: View {
-    let coin : CoinModel
+    let coin: CoinModel
+    let showHoldingsColumn : Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
     }
 }
 
+#Preview {
+        CoinRowView(coin: DeveloperProvider.shared.coin, showHoldingsColumn: true)
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+}
 
-struct CoinRowView_Preview: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin)
+#Preview {
+        CoinRowView(coin: DeveloperProvider.shared.coin, showHoldingsColumn: true)
+            .previewLayout(.sizeThatFits)
+}
+
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryTextColor)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.accentColor)
+                .padding(.leading, 6)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingValue.asCurrencyWith2Decimal())
+                .bold()
+            Text(coin.currentHoldings?.asNumberString() ?? "0.00")
+               
+        }
+        .foregroundStyle(Color.theme.accentColor)
+    }
+    
+    private var rightColumn: some View {
+        
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimal())
+                .bold()
+                .foregroundStyle(Color.theme.accentColor)
+            Text(coin.priceChangePercentage24H?.asPrecentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0.0 > 0)  ?
+                    Color.theme.green :
+                    Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
     }
 }
---
+++ CryptoTracker/Extensions/Double.swift
@@ -0,0 +1,83 @@
+//
+//  Double.swift
+//  CryptoTracker
+//
+//  Created by Abd Elrahman Atallah on 29/07/2024.
+//
+
+import Foundation
+
+
+extension Double {
+    
+    #warning("Maybe logical error here")
+    
+    
+    
+    /// converts a double into currency with 2  decimal places
+    /// ```
+    /// 1234.56 -> $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// converts a double into currency to string  with 2  decimal places
+    /// ```
+    /// 1234.56 -> "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimal() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number ) ?? "0.00"
+    }
+    
+    /// converts a double into currency with 2 - 6 decimal places
+    /// ```
+    /// 1234.56 -> $1,234.56
+    /// ```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    /// converts a double into currency to string  with 2 - 6 decimal places
+    /// ```
+    /// 1234.56 -> "$1,234.56"
+    /// 12.3456 -> "$12,3456"
+    /// 0.123456 -> "$0,123456"
+    /// ```
+    func asCurrencyWith6Decimal() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number ) ?? "0.0"
+    }
+    
+    /// converts a double into to Two decimal fraction
+    /// ```
+    /// 1.2345 -> "1.56"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    /// converts a double into to Two decimal fraction with precent symbol
+    /// ```
+    /// 1.2345 -> "1.23%"
+    /// ```
+    func asPrecentString() -> String {
+        return "\(asNumberString())% "
+    }
+}
